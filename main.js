c281bbae9771d6109894fd928e9d3a491e529caa
YongJang <yj_430@naver.com>
2015-08-13 21:55:49 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
 	$('body').css('overflow','hidden');
 	var height = $(window).height();
 	$('.board').css('height',height);
-	$('#select').draggable();
+	
+	$('.item').draggable({containment:'document', revert:true, start:function(){
+		contents = $(this).text();
+	}
+	});
+
+	$('.board').droppable({hoverClass:'.putIn', accept:'.item',
+	drop:function(){
+		$('.board').append(contents+'<br />');
+	}
+	});
+
 	$('#simulButton').on('click', function(){
 		var $btn = $(this).button('loading');
 		$btn.button('reset')
