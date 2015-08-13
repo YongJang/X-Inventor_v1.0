96cc8f63f690d8ff23d9e84e1b8f4b04ec79772c
YongJang <yj_430@naver.com>
2015-08-14 03:00:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,44 @@
 $(document).ready(function(){
-	$('body').css('overflow','hidden');
-	var height = $(window).height();
-	$('.board').css('height',height);
-	
-	
-
-	
-
-	$('.item').draggable({containment:'document', revert:true, start:function(){
-		$contents = $(this).class;
+	$('body').css('overflow','hidden');	
+	if(1){
+		var height = $(window).height();
+		var width = $(window).width();
+		$('.board').css('height',height);
+	}
+	var selector=0;
+	var x, y;
+	$('.input-item, .output-item').draggable({
+		containment:'document', 
+		revert:true,
+		scope: "tasks",
+		start:function(event, position){
+			x=position.left;
+			contents = $(this).text();
+		},
+		stop:function(){
+			selector=scope;
 		}
 	});
 
-	$('.board').droppable({hoverClass:'.putIn', accept:'.item',
-	drop:function(){
-		$('.ib').append('<div class="ol">'+$contents+'</div>');
-	}
+	$('.board').droppable({
+		hoverClass:'.board',		
+		accept: '.input-item',
+		drop:function(){
+			$('.board').append('<span class="label label-primary">'+selector+'</span>');
+		}		
 	});
 
-	$('.item').on('hover', function(){
-		
+	$('board').droppable({
+		hoverClass:'.board',	
+		accept: '.output-item',
+		drop:function(){
+			$('.board').append('<span class="label label-warning">'+selector+'</span>');
+		}
 	});
-
-	$('#simulButton').on('click', function(){
+	
+	$('#create').on('click', function(){
 		var $btn = $(this).button('loading');
+		$('.board').append('<span class="label label-primary">'+selector+'</span>');
 		$btn.button('reset')
 	});
 });
