3f1ac7849301242033a19b5585c2ec9f302ac569
YongJang <yj_430@naver.com>
2015-08-13 18:04:30 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('body').css('overflow','hidden');
+	var height = $(window).height();
+	$('.board').css('height',height);
+	$('#select').draggable();
+	$('#simulButton').on('click', function(){
+		var $btn = $(this).button('loading');
+		$btn.button('reset')
+	});
+});
