d7ea21cb7038caac4bb0b1f99d62e717d93f89cf
YongJang <yj_430@naver.com>
2015-08-14 01:58:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var height = $(window).height();
 	$('.board').css('height',height);
 	
-	var $id;
+	
 
 	$('.board').droppable({hoverClass:'.board .putIn', accept:'.item',
 	drop:function(){
@@ -14,7 +14,7 @@ $(document).ready(function(){
 	$('li').hover(
     	function(){
      	 $(this).addClass('item');
-     	 $id = $(this).id;
+     	 var $id = $(this).id;
  		},
    		function(){
     	  $(this).removeClass('item');
@@ -24,6 +24,10 @@ $(document).ready(function(){
 	$('.item').draggable({containment:'document', revert:true, start:function(){
 		contents = $(this).text();
 		}
+	});
+
+	$('.item').on('hover', function(){
+		
 	});
 
 	$('#simulButton').on('click', function(){
